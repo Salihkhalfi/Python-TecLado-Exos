5d081823085461fbb4ab459f4d63066a9ee58290
Salah KHALFI <skhalfi19@gmail.com>
2022-06-03 10:44:45 +0200

    l'age en mois et jours

--- main.py
+++ main.py
@@ -3,5 +3,6 @@
 
 user_age = int(input("Entrer votre âge : "))
 months = user_age * 12
+days = user_age * 365
 
-print(f"Votre âge comporte {months} mois !!!")
+print(f"Votre âge comporte {months} mois, ou {days} jours !!!")
